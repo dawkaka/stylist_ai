019dceca8e59b716415a54a8dc1a82fc1bbc5b9a
dawkaka <yousiph77@gmail.com>
2023-03-07 00:36:54 +0000

    firebase shit

--- db/firebase.ts
+++ db/firebase.ts
@@ -6,16 +6,16 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyD9Dd7WpHU_F_xhRRxUuJ55BaWOi55k2Zk",
-    authDomain: "vitual-wardrobe.firebaseapp.com",
-    projectId: "vitual-wardrobe",
-    storageBucket: "vitual-wardrobe.appspot.com",
-    messagingSenderId: "992117308618",
-    appId: "1:992117308618:web:e7237335edf6e6c47b0833",
-    measurementId: "G-14HRMTBSPX"
-};
+
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    appId: process.env.FIREBASE_APP_ID,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+});
 const analytics = getAnalytics(app);
---
+++ process.d.ts
@@ -0,0 +1,22 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        NEXTAUTH_URL: string
+        NEXTAUTH_SECRET: string
+        GITHUB_ID: string
+        GITHUB_SECRET: string
+        FACEBOOK_ID: string
+        FACEBOOK_SECRET: string
+        TWITTER_ID: string
+        TWITTER_SECRET: string
+        GOOGLE_ID: string
+        GOOGLE_SECRET: string
+        DATABASE_URL: string
+        FIREBASE_API_KEY: string
+        FIREBASE_AUTH_DOMAIN: string
+        FIREBASE_PROJECT_ID: string
+        FIREBASE_STORAGE_BUCKET: string
+        FIREBASE_MESSAGING_SENDER_ID: string
+        FIREBASE_APP_ID: string
+        FIREBASE_MEASUREMENT_ID: string
+    }
+}
