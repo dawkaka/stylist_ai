b66cc9ba9e262f4449d82de490f30afa1e37762f
dawkaka <yousiph77@gmail.com>
2023-03-08 19:33:44 +0000

    feat: edit clothing, and delete

--- pages/api/wardrobe/[id].ts
+++ pages/api/wardrobe/[id].ts
@@ -1,8 +1,8 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import formidable from "formidable";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
 import cloudinary from "cloudinary";
 import prisma from "../../../lib/prismadb";
-import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
 
 cloudinary.v2.config({
@@ -13,13 +13,35 @@ cloudinary.v2.config({
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const session = await getServerSession(req, res, authOptions)
+    const { method } = req
+    const { id } = req.query;
     if (!session || !session.user) {
         return res.status(401).json({ message: "Login required" })
     }
-    const { method } = req
 
     switch (method) {
-        case 'POST':
+        case 'PUT':
+            try {
+
+                const { name, color, brand, size, description } = req.body;
+                await prisma.clothes.update({
+                    where: { id: id as string },
+                    data: {
+                        name: name,
+                        color: color,
+                        brand: brand,
+                        size: size,
+                        description: description,
+                    },
+                });
+                res.status(200).json({ message: "Clothing description updated successfully" });
+            } catch (err) {
+                console.log(err);
+                res.status(500).json({ error: "Error updating clothing item" });
+            }
+            break;
+
+        case "PATCH":
             const form = new formidable.IncomingForm();
             form.parse(req, async (err, fields, files) => {
                 if (err) {
@@ -48,40 +70,45 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
                         const result = await cloudinary.v2.uploader.upload(file.filepath);
                         imageUrls.push(result.secure_url);
                     }
+                    if (imageUrls[0]) {
+                        await prisma.clothes.update({ where: { id: id as string }, data: { image: imageUrls[0] } })
+                    }
+                    res.status(200).json({ message: "Clothe image updated successfully" });
 
-                    const clothes = imageUrls.map((imageUrl) => ({
-                        image: imageUrl,
-                        name: "",
-                        color: "",
-                        season: "",
-                        brand: "",
-                        size: "",
-                        description: "",
-                        userId: ""
-                    }));
-
-                    const createdClothes = await prisma.clothes.createMany({
-                        data: clothes,
-                        skipDuplicates: true,
-                    });
-                    res.status(200).json({ message: "Clothes added successfully" });
-                } catch (err) {
+                } catch (error) {
                     res.status(500).json({ error: "Error adding clothes" });
                 }
-            });
+            })
             break;
+        case "DELETE":
+            try {
+                const clothing = await prisma.clothes.findUnique({ where: { id: String(id) } });
+                if (!clothing) {
+                    return res.status(404).json({ message: 'Clothing not found' });
+                }
+                await prisma.clothes.delete({ where: { id: String(id) } });
+                res.status(200).json({ message: 'Clothing deleted successfully' });
+            } catch (err) {
+                console.log(err);
+                res.status(500).json({ error: 'Error deleting clothing' });
+            }
         default:
-            res.setHeader('Allow', ['POST'])
+            res.setHeader('Allow', ['PUT', 'PATCH', "DELETE"])
             res.status(405).end(`Method ${method} Not Allowed`)
     }
 };
 
-
 export const routeConfig = {
     api: {
-        bodyParser: true
-    }
-}
+        method: {
+            POST: {
+                bodyParser: false,
+            },
+            default: {
+                bodyParser: true,
+            },
+        },
+    },
+};
 
-
-export default handler;
+export default handler
