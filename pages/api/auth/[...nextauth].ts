96a646cf727a53438a8a33b0c7f7ba7f6a55be16
dawkaka <yousiph77@gmail.com>
2023-03-18 00:55:55 +0000

    feat: sdf

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -28,13 +28,12 @@ export const authOptions: NextAuthOptions = {
             }
         },
     },
-    pages: {
-        signIn: '/auth/signin',
-        signOut: '/auth/signout',
-        error: '/auth/error', // Error code passed in query string as ?error=
-        verifyRequest: '/auth/verify-request', // (used for check email message)
-        newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-    }
+    // pages: {
+    //     signIn: '/auth/signin',
+    //     signOut: '/auth/signout',
+    //     error: '/auth/error', // Error code passed in query string as ?error=
+    //     verifyRequest: '/auth/verify-request', // (used for check email message)
+    // }
 }
 
 export default NextAuth(authOptions)
