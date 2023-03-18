1a8e3a9d2161b287c2373d17d51e132e9925d869
dawkaka <yousiph77@gmail.com>
2023-03-18 15:15:51 +0000

    privacy and terms

---
+++ pages/privacy.tsx
@@ -0,0 +1,48 @@
+export default function Page() {
+    return (
+
+        <main className="bg-gray-50">
+            <div className="max-w-2xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+                <h1 className="text-3xl font-extrabold text-gray-900 mb-6">Privacy Policy</h1>
+                <div className="prose">
+                    <p>At Virtual Wardrobe, we take your privacy seriously. This Privacy Policy explains how we collect, use, and share the personal information we collect from users of our [Name of Application] application.</p>
+
+                    <h2 className="mt-8">Personal Information We Collect</h2>
+                    <p>We collect the following personal information from our users:</p>
+                    <ul className="list-disc pl-6">
+                        <li>Name</li>
+                        <li>Email address</li>
+                    </ul>
+
+                    <h2 className="mt-8">How We Use Your Personal Information</h2>
+                    <p>
+                        Your use your name to help people identify you and your email as a unique identify for signin purposes.
+                        We may also use your email to send you notifications daily when it's time for your to answer your daily questions so you don't forget, you can unsubscribe any time.
+                    </p>
+
+                    <h2 className="mt-8">Sharing Your Personal Information</h2>
+                    <p>We do not share your personal information with any third parties.</p>
+
+                    <h2 className="mt-8">Data Retention</h2>
+                    <p>
+                        We would only have your personal data as long as you have a Virtual Wardrobe account, if you delete your account, all your data is also deleted.
+                    </p>
+
+                    <h2 className="mt-8">Security</h2>
+                    <p>
+                        We take appropriate measures to protect your personal information from unauthorized access, alteration, disclosure, or destruction.
+                        However, please be aware that no security measures can guarantee absolute protection.
+                        Since we are using google for our auth services you wont' have to worry about your password getting leaked from us.
+                    </p>
+
+                    <h2 className="mt-8">Changes to This Privacy Policy</h2>
+                    <p>We reserve the right to update or modify this Privacy Policy at any time. Please check this page periodically to ensure that you are aware of any changes.</p>
+
+                    <h2 className="mt-8">Contact Us</h2>
+                    <p>If you have any questions about this Privacy Policy or our data practices, please contact us at <a href="mailto:yousiph77@gmail.com">yousiph77@gmail.com</a>.</p>
+                </div>
+            </div>
+        </main>
+
+    )
+}
---
+++ pages/term.tsx
@@ -0,0 +1,30 @@
+export default function Page() {
+    return (
+
+        <main className="bg-gray-50">
+            <div className="max-w-2xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+                <h1 className="text-3xl font-extrabold text-gray-900 mb-6">Terms of Service</h1>
+
+                <div className="prose">
+                    <p>Welcome to Virtual Wardrobe ("we," "us," "our"). By accessing or using the Service, you agree to be bound by these Terms of Service and all applicable laws and regulations. If you do not agree with these Terms of Service, you are not permitted to access or use the Service.</p>
+
+                    <h2 className="mt-8">Description of Service</h2>
+                    <p>The Service provides couples with five questions daily to help them know each other better. We reserve the right to modify or discontinue the Service at any time without notice.</p>
+
+                    <h2 className="mt-8">User Conduct</h2>
+                    <p>By accessing or using the Service, you agree to comply with all applicable laws and regulations. You also agree not to:</p>
+                    <ul className="list-disc pl-6">
+                        <li>Use the Service in any manner that could interfere with, disrupt, negatively affect, or inhibit other users from fully enjoying the Service;</li>
+                        <li>Use the Service for any illegal or unauthorized purpose;</li>
+                        <li>Attempt to circumvent any content-filtering techniques we employ, or attempt to access any feature or area of the Service that you are not authorized to access;</li>
+                        <li>Engage in any harassing, intimidating, or predatory conduct;</li>
+                        <li>Use the Service to transmit any spam, viruses, or other harmful computer code, files, or programs;</li>
+                        <li>Impersonate any person or entity or misrepresent your affiliation with a person or entity;</li>
+                        <li>Violate any applicable laws or regulations, or encourage or promote any illegal activity.</li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+
+    )
+}
