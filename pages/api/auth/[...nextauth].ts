117985950070a83e63725686692ba111a36133ce
dawkaka <yousiph77@gmail.com>
2023-03-18 15:41:13 +0000

    change env variable need to push somethign

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -36,7 +36,7 @@ export const authOptions: NextAuthOptions = {
     pages: {
         signIn: '/auth/signin',
         signOut: '/auth/signout',
-        error: '/auth/error', // Error code passed in query string as ?error=
+        error: '/auth/error',
         verifyRequest: '/auth/verify-request', // (used for check email message)
     }
 }
