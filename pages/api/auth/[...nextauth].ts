7d83ea12412ebba8d785873557d11897e07177fc
dawkaka <yousiph77@gmail.com>
2023-03-18 11:06:56 +0000

    das database_url

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -28,12 +28,12 @@ export const authOptions: NextAuthOptions = {
             }
         },
     },
-    // pages: {
-    //     signIn: '/auth/signin',
-    //     signOut: '/auth/signout',
-    //     error: '/auth/error', // Error code passed in query string as ?error=
-    //     verifyRequest: '/auth/verify-request', // (used for check email message)
-    // }
+    pages: {
+        signIn: '/auth/signin',
+        signOut: '/auth/signout',
+        error: '/auth/error', // Error code passed in query string as ?error=
+        verifyRequest: '/auth/verify-request', // (used for check email message)
+    }
 }
 
 export default NextAuth(authOptions)
