d0e7db80096aadc1da1547178ac0d4cc9f9d40ae
dawkaka <yousiph77@gmail.com>
2023-03-19 14:39:28 +0000

    title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,8 +17,11 @@ export default function App({
     <SessionProvider session={session}>
       <Head>
         <meta charSet="utf-8" /><meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8" /><meta name="referrer" content="always" />
-        <meta http-equiv="X-UA-Compatible" /><link rel="canonical" href="https://wwww.virtualwardrobe.co" />
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <meta name="referrer" content="always" />
+        <meta http-equiv="X-UA-Compatible" />
+        <link rel="canonical" href="https://wwww.virtualwardrobe.co" />
+        <title>Virtual wardrobe and AI stylist</title>
         <meta name="twitter:label1" content="Virtual Wardrobe" /><meta name="twitter:domain" content="virtualwardrobe.co" />
         <meta name="twitter:card" content="summary_large_image" /><meta name="description" content="Your entire wardrobe in one place and let our Ai stylist recommend outfits for you." />
         <meta name="keywords" content="virtual wardrobe, ai, stylist, outfit recommendation" />
